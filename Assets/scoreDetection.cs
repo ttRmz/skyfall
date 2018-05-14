46df398a89f167c7a60f28b4a6e1893452adb095
Tristan RAMIREZ <tristan.rami@gmail.com>
2018-05-14 22:43:44 +0200

    score

---
+++ Assets/scoreDetection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreDetection : MonoBehaviour {
+
+	[SerializeField]
+	private int score = 0;
+
+	void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "Enemy")
+        {
+            score ++;
+			GameObject.Find("Canvas").GetComponent<canvasScript>().MajScore(score);
+        }
+    }
+}
