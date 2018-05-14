3117ce016f9bfd05448a2b18bae26d809c652f4e
Tristan RAMIREZ <tristan.rami@gmail.com>
2018-05-14 19:06:00 +0200

    firsts assets: player + plate + evironement

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    float MultSpeed = 2000;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float Speed = Time.deltaTime * MultSpeed;
+
+        Rigidbody r = GetComponent<Rigidbody>();
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            r.AddForce(new Vector3(-Speed, 0, 0));
+        if (Input.GetKey(KeyCode.RightArrow))
+            r.AddForce(new Vector3(Speed, 0, 0));
+
+	}
+}
---
+++ Assets/enemyDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDestroy : MonoBehaviour {
+    
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
